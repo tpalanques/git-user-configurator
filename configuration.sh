6508b6e537c5a0ed48d0bbf8e22b6fba3772bf7e
Toni Palanques <tpalanques@gmail.com>
2022-10-24 15:58:41 +0200

    Update documentation

--- configuration.sh
+++ configuration.sh
@@ -16,9 +16,10 @@ userDefaultConfig=$(system.getRootPath)/default/user.cfg
 
 #==========================================================================
 #
-#   DESCRIPTION:  Reads configuration in a given file
+#   DESCRIPTION:  Reads configuration in a given file. If not found string
+#                 "__UNDEFINED__" is echoed
 #       PRIVACY:  PRIVATE
-#         USAGE:  system.getRootPath "${CONFIGURATION_NAME}"
+#         USAGE:  configuration.read_file "${CONFIGURATION_FILE}" "${CONFIGURATION_NAME}"
 #          ARG1:  File where to get configuration from
 #          ARG2:  Configuration name to get
 #
@@ -27,6 +28,15 @@ configuration.read_file() {
   (grep -E "^${2}=" -m 1 "${1}" 2>/dev/null || echo "VAR=__UNDEFINED__") | head -n 1 | cut -d '=' -f 2-
 }
 
+#==========================================================================
+#
+#   DESCRIPTION:  Reads configuration from the user configuration or from
+#                 the default file if it couldn't be found
+#       PRIVACY:  PUBLIC
+#         USAGE:  configuration.get "${CONFIGURATION_NAME}"
+#          ARG2:  Configuration name to get
+#
+#==========================================================================
 configuration.get() {
   local configName
   local configValue
