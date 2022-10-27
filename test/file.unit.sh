5af62f4b082340c321fc5db725dfdf330550b465
Toni Palanques <tpalanques@gmail.com>
2022-10-27 13:15:19 +0200

    Add unit tests for path existance and removal

--- test/file.unit.sh
+++ test/file.unit.sh
@@ -19,7 +19,7 @@ file.unit.create_a_folder(){
     rm -rf ${basePath}
 }
 
-file.unit.path_exists(){
+file.unit.folder_exists(){
   # Arrange
   local path="/tmp/"
 
@@ -27,7 +27,7 @@ file.unit.path_exists(){
   unit.assertEqual "$(file.pathExists ${path})" "1" "${FUNCNAME[0]}" "Folder ${path} does not exist"
 }
 
-file.unit.path_does_not_exist(){
+file.unit.folder_does_not_exist(){
   # Arrange
   local path="/notExisting/"
 
@@ -35,7 +35,23 @@ file.unit.path_does_not_exist(){
   unit.assertEqual "$(file.pathExists ${path})" "0" "${FUNCNAME[0]}" "Folder ${path} does exist but it shouldn't"
 }
 
+file.unit.remove_an_existing_folder(){
+  # Arrange
+  local path="/tmp/test/file_unit_create_folder/new/folder"
+  local basePath="/tmp/test"
+  mkdir -p ${basePath}
+
+  # Act
+  file.removePath "${path}"
+
+  # Assert
+  unit.assertEqual "$(file.pathExists ${path})" "0" "${FUNCNAME[0]}" "Folder ${path} is present but we removed it"
+
+  # TearDown
+    rm -rf ${basePath}
+}
+
 file.unit.create_a_folder
-file.unit.path_exists
-file.unit.path_does_not_exist
-#file.unit.remove_existing_path
+file.unit.folder_exists
+file.unit.folder_does_not_exist
+file.unit.remove_an_existing_folder
