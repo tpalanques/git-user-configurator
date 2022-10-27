fa8e8747d41530cdda8d7e6cdea8e9c366e46277
Toni Palanques <tpalanques@gmail.com>
2022-10-27 13:11:22 +0200

    Add unit tests for path existance

--- test/file.unit.sh
+++ test/file.unit.sh
@@ -13,12 +13,29 @@ file.unit.create_a_folder(){
   file.createPath "${path}"
 
   # Assert
-  unit.assertEqual "$(file.pathExists ${path})" "1" "${FUNCNAME[0]}" "Folder ${path} is not present"
+  unit.assertEqual "$(file.pathExists ${path})" "1" "${FUNCNAME[0]}" "Folder ${path} is not present but we created it"
 
   # TearDown
     rm -rf ${basePath}
 }
 
+file.unit.path_exists(){
+  # Arrange
+  local path="/tmp/"
+
+  # Assert
+  unit.assertEqual "$(file.pathExists ${path})" "1" "${FUNCNAME[0]}" "Folder ${path} does not exist"
+}
+
+file.unit.path_does_not_exist(){
+  # Arrange
+  local path="/notExisting/"
+
+  # Assert
+  unit.assertEqual "$(file.pathExists ${path})" "0" "${FUNCNAME[0]}" "Folder ${path} does exist but it shouldn't"
+}
+
 file.unit.create_a_folder
-#file.unit.path_exists
+file.unit.path_exists
+file.unit.path_does_not_exist
 #file.unit.remove_existing_path
